a65c9e766c97ea1aed84a16095559957d3e6543b
madhavgupta19 <104672414+madhavgupta19@users.noreply.github.com>
2024-04-20 21:06:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Madhav")
+
+
+let name = "madhav" // string data type
+let age = 18  // number datatype
+let isLoggedIn = false  // boolean data type
+let state; // undefined
+
+// range of number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (representation of empty value)
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof "Madhav"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object (null is an object)
