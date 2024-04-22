d1ae5f384577c7d17d7b46ce1b9144a279930615
madhavgupta19 <104672414+madhavgupta19@users.noreply.github.com>
2024-04-22 09:57:25 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,7 +31,7 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue);  -3
 
 // console.log(2+2);
 // console.log(2-2);
@@ -44,17 +44,18 @@ let str1 = "hello"
 let str2 = " madhav"
 
 let str3 = str1 + str2
-// console.log(str3);
+// console.log(str3); hello madhav
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); 12
+// console.log(1 + "2"); 12
+// console.log("1" + 2 + 2); 122 
+// console.log(1 + 2 + "2"); 32
 
 // console.log( (3 + 4) * 5 % 3);
-
-// console.log(+true);
-// console.log(+"");
+ 
+// console.log(+true); 1
+// console.log(true+); error
+// console.log(+""); 0
 
 let num1, num2, num3
 
